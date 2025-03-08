09fc3f4b4a6a7671a839f3f3994a1b11e6e90852
Dimitar Gluharski <dimitargluharski@gmail.com>
2025-03-08 18:18:02 +0200

    add hot topic label

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,7 +16,6 @@ const posts = [
 export const HomePage = () => {
   const [topics, setTopics] = useState(posts);
   const [term, setTerm] = useState('');
-  const [showMore, setShowMore] = useState(3);
 
   const handleOnChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTerm(event.target.value);
@@ -34,57 +33,56 @@ export const HomePage = () => {
     ))
   }
 
-  const handleComment = (postId: number, title: string) => {
-    console.log(postId);
-    console.log(title);
-  }
-
-  const handleShowMoreTopics = () => {
-    setShowMore((oldValue) => oldValue + showMore);
-  }
-
   return (
     <div className="max-w-3xl mx-auto p-4 flex flex-col shadow-md">
       <InputField handleOnChangeInput={handleOnChangeInput} term={term} />
 
       <div className="flex flex-col gap-2">
         {topics.sort((a, b) => b.votes - a.votes).map((topic) => (
-          <div key={topic.id} className="flex flex-col gap-2 border rounded-md p-2">
-            <div className="flex gap-1">
-              <span className="font-bold italic">
-                {topic.title}
-              </span>
+          <div key={topic.id} className="flex text-center gap-2 border rounded-md p-2">
+            <div className="w-full flex flex-col">
+              <div className="flex gap-1">
+                <span className="font-bold italic">
+                  {topic.title}
+                </span>
+              </div>
+
+              <div className="flex gap-2">
+                <div className="flex gap-1">
+                  <span>
+                    author:
+                  </span>
+
+                  <span className="italic font-bold">
+                    {topic.author}
+                  </span>
+                </div>
+
+                <div className="font-bold">
+                  {topic.votes}
+                </div>
+
+                <button onClick={() => handleComment(topic.id, topic.title)} className="hover:cursor-pointer">
+                  <Link to={`/${topic.id}`}>
+                    <IoChatboxEllipsesOutline className="w-5 h-5" />
+                  </Link>
+                </button>
+
+                <button onClick={() => handlePositiveVote(topic.id)} className="hover:cursor-pointer">
+                  <GoThumbsup className="w-5 h-5" />
+                </button>
+
+                <button onClick={(() => handleNegaviteVote(topic.id))} className="hover:cursor-pointer">
+                  <GoThumbsdown className="w-5 h-5" />
+                </button>
+
+              </div>
             </div>
 
-            <div className="flex gap-2">
-              <div className="flex gap-1">
-                <span>
-                  author:
-                </span>
-
-                <span className="italic font-bold">
-                  {topic.author}
-                </span>
-              </div>
-
-              <div className="font-bold">
-                {topic.votes}
-              </div>
-
-              <button onClick={() => handleComment(topic.id, topic.title)} className="hover:cursor-pointer">
-                <Link to={`/${topic.id}`}>
-                  <IoChatboxEllipsesOutline className="w-5 h-5" />
-                </Link>
-              </button>
-
-              <button onClick={() => handlePositiveVote(topic.id)} className="hover:cursor-pointer">
-                <GoThumbsup className="w-5 h-5" />
-              </button>
-
-              <button onClick={(() => handleNegaviteVote(topic.id))} className="hover:cursor-pointer">
-                <GoThumbsdown className="w-5 h-5" />
-              </button>
-
+            <div className="flex justify-end w-full">
+              <span className={`${topic.votes >= 8 ? 'bg-red-400' : ''} p-2 rounded`}>
+                {topic.votes >= 8 ? 'hot topic' : ''}
+              </span>
             </div>
           </div>
         ))}
